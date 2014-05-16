ca291d2df9b79b1427180573a9b217297bed7ba2
Adnan Tavarez <adnanitt@gmail.com>
2014-05-16 16:15:27 -0400

    Pushing new changes

---
+++ source/app.specs/IQueryDataFromADatabaseSpecs.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using app.web.core;
+using developwithpassion.specifications.rhinomocks;
+using Machine.Specifications;
+
+namespace app.specs
+{
+    public class query_from_database_concern : Observes<IQueryDataFromDatabase >
+    {
+
+    }
+
+    public class when_runing_query : query_from_database_concern
+    {
+
+
+
+
+        Because b = () =>
+        {
+
+        };
+
+        It should_open_the_connection = () =>
+        {
+
+        };
+
+
+        It should_run_the_send_query = () =>
+        {
+
+        };
+
+
+
+    }
+
+    internal class QueryDataTest : IQueryDataFromDatabase
+    {
+        public IDataReader run_query(IDbConnection connection, string query)
+        {
+            connection.Open();
+            var command =connection.CreateCommand();
+            command.CommandText = query;
+            return command.ExecuteReader();
+        }
+    }
+}
---
+++ source/app/web/core/IQueryDataFromDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace app.web.core
+{
+    public interface IQueryDataFromDatabase
+    {
+        IDataReader run_query(IDbConnection connection, string query);
+    }
+}
